a9b174d174f4ad640f8e6b865862c496bc4a7ff7
Matheus Borghi <70172644+mtsborghi@users.noreply.github.com>
2020-11-30 17:25:32 -0300

    Update code.c

--- code.c
+++ code.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <pthread.h>
 
 int main(void){
 
